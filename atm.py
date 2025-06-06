ca11aa12f5ffdc3b0a3081b1c47f6edd1723104d
DESKTOP-NCSJNG2\Me <zkzktldk1234@gmail.com>
2025-06-06 22:16:36 +0900

    ✨ 출금 기능 추가
    사용자가 2(출금)을 입력하고 출금 금액을 입력하면 통장 잔액에서 차감하는 기능 구현

--- atm.py
+++ atm.py
@@ -7,6 +7,13 @@ while True:
     #사용자가 기능 선택
     num = input("사용하실 기능의 번호를 입력해주세요. (1.입금, 2.출금, 3.영수증 보기, 4.종료) : ")
     
+    #출금 기능 선택(2번)
+    if num == "2":
+        withdraw_amount = int(input("출금할 금액을 입력해주세요 : "))
+        withdraw_amount = min(balance, withdraw_amount)
+        balance -= withdraw_amount
+        print(f"출금하신 금액은 {withdraw_amount}원입니다. 현재 잔액은 {balance}원입니다.")
+
     #입금 기능 선택(1번)
     if num == "1":
         deposit_amount = int(input("입금할 금액을 입력해주세요 : "))
