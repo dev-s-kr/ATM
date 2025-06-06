23f7b48d15d2b0854eb0044a2414875434a72ffd
DESKTOP-NCSJNG2\Me <zkzktldk1234@gmail.com>
2025-06-06 21:53:45 +0900

    🐛 코드 수정
    사용자 기능 선택 부분을 실수로 삭제해 다시 추가.

--- atm.py
+++ atm.py
@@ -1,18 +1,17 @@
 #잔액 초기화
 balance = 1000
 
-
 #조건이 True면 계속 작동
 while True:
 
     #사용자가 기능 선택
-    deposit_amount = int(input("입금할 금액을 입력해주세요 : "))
-    balance += deposit_amount
-    print(f"입금하신 금액은 {deposit_amount}원입니다. 현재 잔액은 {balance}원입니다.")
+    num = input("사용하실 기능의 번호를 입력해주세요. (1.입금, 2.출금, 3.영수증 보기, 4.종료) : ")
     
     #입금 기능 선택(1번)
     if num == "1":
-        print("입금")
+        deposit_amount = int(input("입금할 금액을 입력해주세요 : "))
+        balance += deposit_amount
+        print(f"입금하신 금액은 {deposit_amount}원입니다. 현재 잔액은 {balance}원입니다.")
 
     #종료 기능 선택(4번)
     if num == "4":
